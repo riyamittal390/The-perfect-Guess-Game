2338773f03da49aac8058e3e86f53a5026c97558
Riya <riyaamittal39@gmail.com>
2026-06-22 21:59:17 +0530

    My final commit

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ a = -1
 guesses = 0
 
 while (a != n):
-    guesses += 1
     a = int(input("Guess the number : "))
 
     if (a > n):
         print ("Lower number please!")
-    else:
+    elif (a < n):
         print ("Higher number please!")
+        guesses += 1
 
 print (f"You have guessed the number {n} corectly in {guesses} guesses!")
